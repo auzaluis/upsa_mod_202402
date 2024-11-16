81765dbb398624f420dc3b8b12cddc6f1cbad271
Luis Auza <luis.auza@gmail.com>
2024-11-16 15:31:55 +0000

    WIP

--- personalidad/02-script-PCA-personalidad.R
+++ personalidad/02-script-PCA-personalidad.R
@@ -5,7 +5,7 @@ library(plotly)
 
 frases3 <- as.vector(frases2)
 
-# Matriz de correlaciones
+# Matriz de correlaciones ----
 r <- cor(df6 |> select(all_of(frases3)),
          method = "spearman")
 
@@ -24,3 +24,155 @@ ggplotly(
         panel.grid.major.y = element_blank())
 
   )
+
+
+
+# PCA: Principal Component Analysis ----
+
+## Dimensión: extraversion ----
+
+### Definiendo vector con la variables
+social <- frases3[c(20,23)]
+
+### Crear la dimensión
+PCA_social <- FactoMineR::PCA(
+  df6 |> select(all_of(social)),
+  ncp = 1
+)
+
+### Eigenvalues
+PCA_social$eig
+
+
+### Correlación entre las vars y la dimensión
+PCA_social$var$cor
+
+
+### Valores de la dimensión
+PCA_social$ind$coord
+
+
+### Comparación dim vs vars
+tibble(
+  Dim = PCA_social$ind$coord,
+  df6 |> select(all_of(social))
+) |> View()
+
+
+
+## Dimensión: consumismo ----
+
+### Definiendo vector con la variables
+consumismo <- frases3[c(2,10,16)]
+
+### Crear la dimensión
+PCA_consumismo <- FactoMineR::PCA(
+  df6 |> select(all_of(consumismo)),
+  ncp = 1
+)
+
+### Eigenvalues
+PCA_consumismo$eig
+
+
+### Correlación entre las vars y la dimensión
+PCA_consumismo$var$cor
+
+### Comparación dim vs vars
+tibble(
+  Dim = PCA_consumismo$ind$coord,
+  df6 |> select(all_of(consumismo))
+) |> View()
+
+
+
+## Dimensión: tradición ----
+
+### Definiendo vector con la variables
+tradicion <- frases3[c(9,12,14)]
+
+### Crear la dimensión
+PCA_tradicion <- FactoMineR::PCA(
+  df6 |> select(all_of(tradicion)),
+  ncp = 1
+)
+
+### Eigenvalues
+PCA_tradicion$eig
+
+
+### Correlación entre las vars y la dimensión
+PCA_tradicion$var$cor
+
+
+
+## Dimensión: intraversión ----
+
+### Definiendo vector con la variables
+intraversion <- frases3[c(21,22)]
+
+### Crear la dimensión
+PCA_intraversion <- FactoMineR::PCA(
+  df6 |> select(all_of(intraversion)),
+  ncp = 1
+)
+
+### Eigenvalues
+PCA_intraversion$eig
+
+
+### Correlación entre las vars y la dimensión
+PCA_intraversion$var$cor
+
+
+
+## Dimensión: innovación ----
+
+### Definiendo vector con la variables
+innovacion <- frases3[c(7,8,19)]
+
+### Crear la dimensión
+PCA_innovacion <- FactoMineR::PCA(
+  df6 |> select(all_of(innovacion)),
+  ncp = 1
+)
+
+### Eigenvalues
+PCA_innovacion$eig
+
+
+### Correlación entre las vars y la dimensión
+PCA_innovacion$var$cor
+
+
+
+# Data frame con las dimensiones
+
+df10 <- df6 |> 
+  mutate(
+    social       = PCA_social$ind$coord *-1,
+    consumismo   = PCA_consumismo$ind$coord,
+    tradicion    = PCA_tradicion$ind$coord *-1,
+    intraversion = PCA_intraversion$ind$coord *-1,
+    innovacion   = PCA_innovacion$ind$coord *-1,
+  )
+
+dimensiones <- c(
+  "social",
+  "consumismo",
+  "tradicion",
+  "intraversion",
+  "innovacion"
+)
+
+df10 |> select(all_of(dimensiones))
+
+
+
+
+
+
+
+
+
+

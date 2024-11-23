124314b8b25aa3f0ca1ff1065a9706e659a46f9f
Luis Auza <luis.auza@gmail.com>
2024-11-23 16:08:08 +0000

    wip

---
+++ groceries/script-groceries.R
@@ -0,0 +1,106 @@
+
+library(gsheet)
+library(tidyverse)
+library(arules)
+library(arulesViz)
+library(viridis)
+
+# Importar la base de datos
+url <- "https://docs.google.com/spreadsheets/d/1NTjA8nrmcWltvZn4oq5KJK7-R4Mb-_is_5vVsoBDCv0/edit?pli=1&gid=0#gid=0"
+
+df <- read.csv(
+  text = gsheet2text(url, format = "csv"),
+  stringsAsFactors = F
+)
+
+
+# Creando canastas
+## id canasta
+df2 <- df |> 
+  mutate(basket_id = paste(Member_number, Date, sep = "_")) |> 
+  select(basket_id, itemDescription)
+
+## Convirtiendo a lista
+df_to_list <- split(
+  x = df2$itemDescription,
+  f = df2$basket_id
+)
+
+## Convirtiendo a canasta
+list_to_basket <- as(
+  object = df_to_list,
+  Class = "transactions"
+)
+
+class(list_to_basket)
+
+
+# Análisis descriptivo ----
+
+itemFrequencyPlot(
+  x = list_to_basket,
+  topN = 20,
+  horiz = T
+)
+
+
+# Reglas ----
+
+rules <- apriori(
+  data = list_to_basket,
+  parameter = list(
+    supp = 0.001,
+    conf = 0.15,
+    minlen = 2,
+    maxlen = 3
+  )
+)
+
+inspect(rules)
+plot(
+  rules,
+  method = "graph",
+  engine = "htmlwidget"
+)
+
+
+# Fijar la mano derecha
+
+rules_rhs <- apriori(
+  data = list_to_basket,
+  parameter = list(
+    supp = 0.001,
+    conf = 0.07,
+    minlen = 2,
+    maxlen = 2
+  ),
+  appearance = list(
+    rhs = "sausage",
+    default = "lhs"
+  )
+)
+
+inspect(rules_rhs)
+
+
+# Fijar la mano izquierda
+
+rules_lhs <- apriori(
+  data = list_to_basket,
+  parameter = list(
+    supp = 0.001,
+    conf = 0.05,
+    minlen = 2,
+    maxlen = 2
+  ),
+  appearance = list(
+    lhs = "whole milk",
+    default = "rhs"
+  )
+)
+
+inspect(rules_lhs)
+
+
+
+
--- personalidad/03-script-clustering-personalidad.R
+++ personalidad/03-script-clustering-personalidad.R
@@ -1,3 +1,87 @@
 
 library(NbClust)
+library(FactoMineR)
+
+# Iteración
+
+clustering <- NbClust(
+  data = df10 |> select(all_of(dimensiones)),
+  distance = 'euclidean',
+  method = 'ward.D2',
+  index = 'dunn'
+)
+
+clustering
+
+clustering2 <- NbClust(
+  data = df10 |> select(all_of(dimensiones)),
+  distance = 'euclidean',
+  method = 'ward.D2',
+  index = 'dunn',
+  min.nc = 5,
+  max.nc = 5
+)
+
+clustering2
+
+
+
+# Tamaño de los segmentos
+table(clustering2$Best.partition)
+prop.table(table(clustering2$Best.partition))
+
+
+
+# Agregar la columna con el cluster al data frame
+clustering2$Best.partition
+
+df11 <- df10 |>
+  mutate(segmento = clustering2$Best.partition)
+
+
+# Analizando lo segmentos
+
+## Cruce de variables
+
+df11 |> 
+  group_by(segmento) |> 
+  summarise(
+    social       = mean(social),
+    consumismo   = mean(consumismo),
+    tradicion    = mean(tradicion),
+    intraversion = mean(intraversion),
+    innovacion   = mean(innovacion)
+  )
+
+
+df12 <- df11 |> 
+  mutate_at(.vars = dimensiones,
+            .funs = rescale)
+
+
+df13 <- df12 |> 
+  group_by(segmento) |> 
+  summarise(
+    social       = mean(social),
+    consumismo   = mean(consumismo),
+    tradicion    = mean(tradicion),
+    intraversion = mean(intraversion),
+    innovacion   = mean(innovacion)
+  ) |> 
+  column_to_rownames("segmento")
+
+
+## Análisis de correspondencias
+FactoMineR::CA(df13)
+
+
+
+
+
+
+
+
+
+
+
 
